29875d6f977fa4b04dc1b51d6096020d9ec5b42d
Marco Carmona <marcocarmonadev@outlook.com>
2024-09-19 13:33:36 -0600

    Add Base metadata

--- backend/source/frameworks_and_drivers/database/migrations/env.py
+++ backend/source/frameworks_and_drivers/database/migrations/env.py
@@ -3,28 +3,16 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import create_engine
 
+from source.entities import Base
 from source.frameworks_and_drivers import database
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
-settings = database.Settings()
+settings = database.Settings()  # type: ignore
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
