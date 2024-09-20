0012c7f0a9d7b563d96744d2da421d091030ede4
Marco Carmona <marcocarmonadev@outlook.com>
2024-09-20 10:08:38 -0600

    Add get_health endpoint

--- backend/source/frameworks_and_drivers/web/routers/__init__.py
+++ backend/source/frameworks_and_drivers/web/routers/__init__.py
@@ -5,4 +5,15 @@ from .v1 import router as v1_router
 router = APIRouter(
     prefix="/api",
 )
+
+
+@router.get(
+    path="/health",
+)
+def get_health():
+    return {
+        "health": "OK",
+    }
+
+
 router.include_router(v1_router)
